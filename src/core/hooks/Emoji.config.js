cbfb0189b57db7dc9c4ac5474767cafb02a394ab
Ankio <ankio@ankio.net>
2023-12-03 11:16:53 +0800

    fix:样式美化

--- src/core/hooks/Emoji.config.js
+++ src/core/hooks/Emoji.config.js
@@ -1798,28 +1798,3 @@ export const gfmUnicode = {
     zzz: '1f4a4',
   },
 };
-
-export const githubCustom = {
-  defaultURL: 'https://github.githubassets.com/images/icons/emoji/${code}.png?v8',
-  emojis: {
-    atom: 'atom',
-    basecamp: 'basecamp',
-    basecampy: 'basecampy',
-    bowtie: 'bowtie',
-    electron: 'electron',
-    feelsgood: 'feelsgood',
-    finnadie: 'finnadie',
-    goberserk: 'goberserk',
-    godmode: 'godmode',
-    hurtrealbad: 'hurtrealbad',
-    neckbeard: 'neckbeard',
-    octocat: 'octocat',
-    rage1: 'rage1',
-    rage2: 'rage2',
-    rage3: 'rage3',
-    rage4: 'rage4',
-    shipit: 'shipit',
-    suspect: 'suspect',
-    trollface: 'trollface',
-  },
-};
