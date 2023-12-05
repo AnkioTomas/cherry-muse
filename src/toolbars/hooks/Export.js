ed38165a3e85ae1957448b2d8e50c5e3bf45b1f9
Ankio <ankio@ankio.net>
2023-12-05 22:07:25 +0800

    :zap: 长图这种八百年都用不上的玩意删了

--- src/toolbars/hooks/Export.js
+++ src/toolbars/hooks/Export.js
@@ -23,7 +23,6 @@ export default class Export extends MenuBase {
     this.updateMarkdown = false;
     this.subMenuConfig = [
       { noIcon: true, name: 'exportToPdf', onclick: this.bindSubClick.bind(this, 'pdf') },
-      { noIcon: true, name: 'exportScreenshot', onclick: this.bindSubClick.bind(this, 'screenShot') },
       { noIcon: true, name: 'exportMarkdownFile', onclick: this.bindSubClick.bind(this, 'markdown') },
       { noIcon: true, name: 'exportHTMLFile', onclick: this.bindSubClick.bind(this, 'html') },
     ];
