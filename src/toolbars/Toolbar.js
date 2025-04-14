9b52a36ed3a4ab3db7d7da59113904a4b78deedb
ankio <ankio@ankio.net>
2025-04-14 18:08:13 +0800

    :heavy_plus_sign: (toolbars): 添加 `mac` 导入以支持特定浏览器功能

--- src/toolbars/Toolbar.js
+++ src/toolbars/Toolbar.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { mac } from 'codemirror/src/util/browser';
 import HookCenter from './HookCenter';
 import Event from '@/Event';
 import { createElement } from '@/utils/dom';
